7deaeaef8ad714e564949076f34079f2d59a5684
Luiz Romário Filho <luizromariofilho@gmail.com>
2015-04-15 21:23:30 -0300

    Add angular

--- web-app/app/app.js
+++ web-app/app/app.js
@@ -1,3 +1,19 @@
 /**
  * Created by Luiz Romario Filho on 15/04/2015.
  */
+var bookApp = angular.module('bookApp', []);
+
+bookApp.controller('BookCtrl', function ($scope) {
+    $scope.books = [
+        {'title': 'Nexus S',
+            'author': 'Fast just got faster with Nexus S.',
+        'price': 30.00},
+        {'title': 'Nexus S',
+            'author': 'Fast just got faster with Nexus S.',
+            'price': 30.00},
+        {'title': 'Nexus S',
+            'author': 'Fast just got faster with Nexus S.',
+            'price': 30.00}
+
+    ];
+});
