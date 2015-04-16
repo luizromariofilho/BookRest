264e9828b373bc512badde22de6b3aea22b0a3b1
Luiz Romário Filho <luizromariofilho@gmail.com>
2015-04-16 13:59:52 -0300

    Creating base structure.

--- web-app/app/modules/book/Book.js
+++ web-app/app/modules/book/Book.js
@@ -1,3 +1,25 @@
 /**
  * Created by Luiz Romario Filho on 15/04/2015.
  */
+bookApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+        when('/books', {
+            templateUrl: 'partials/book-list.html',
+            controller: 'BookListController'
+        }).
+        when('/books/:id/view', {
+            templateUrl: 'partials/book-view.html',
+            controller: 'BookViewController'
+        }).
+        when('/books/:id/edit', {
+            templateUrl: 'partials/book-form.html',
+            controller: 'BookEditController'
+        }).
+        when('/books/new', {
+            templateUrl: 'partials/book-form.html',
+            controller: 'BookNewController'
+        }).
+        otherwise({
+            redirectTo: '/books'
+        });
+}])
