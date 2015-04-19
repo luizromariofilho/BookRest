e9e0c3c36fdbd3f44c93a08fc60fcb6dc466f6d5
Luiz Romário Filho <luizromariofilho@gmail.com>
2015-04-19 01:22:30 -0300

    Fix bug on click in tr on table

--- web-app/app/modules/book/controllers/BookListController.js
+++ web-app/app/modules/book/controllers/BookListController.js
@@ -5,14 +5,18 @@ bookApp.controller('BookListController',['$scope','Books','$state', function($sc
         $scope.books = Books;
 
         $scope.view = function(book){
-                $state.go("view",{id:book.id})
+                $state.go("view",{id:book.id});
+        }
+
+        $scope.edit = function(book){
+                $state.go("edit",{id:book.id});
         }
 
         $scope.delete = function(book){
                 if(confirm("Deseja excluir o livro: " + book.title + " ?"))
                 book.$delete({id:book.id},function(){
                         alert("Excluído com sucesso!");
-                        $state.go('list', {}, {reload: true})
+                        $state.go('list', {}, {reload: true});
                 })
         }
 }]);
