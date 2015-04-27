c03f594d140225593002e5c7857a1012e6250543
Luiz Romário Filho <luizromariofilho@gmail.com>
2015-04-27 00:12:22 -0300

    Try animate.

--- web-app/app/app.js
+++ web-app/app/app.js
@@ -1,4 +1,4 @@
 /**
  * Created by Luiz Romario Filho on 15/04/2015.
  */
-var bookApp = angular.module('bookApp',['ngResource', 'ui.router', 'bookServices', 'bookDirectives', 'angularFileUpload','ngFileUpload']);
+var bookApp = angular.module('bookApp',['ngResource', 'ngAnimate', 'ui.router', 'bookServices', 'bookDirectives', 'angularFileUpload','ngFileUpload']);
