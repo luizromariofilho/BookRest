bfecc454a60c2ae6cfc721198746df480bc0064c
luizromariofilho <luizromariofilho@gmail.com>
2015-05-20 14:10:37 -0300

    refactor on bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,8 +3,8 @@ import bookrest.Book
 class BootStrap {
 
     def init = { servletContext ->
-        Book.count() > 0 ? null : new Book(title:"Java Persistence with Hibernate", author:"Gavin King", price:99.00).save()
-        Book.count() > 0 ? null : new Book(title:"Spring Live", author:"Matt Raible", price:29.00).save()
+        Book.findOrSaveByTitleAndAuthor("Java Persistence with Hibernate", "Gavin King")
+        Book.findOrSaveByTitleAndAuthor("Spring Live", "Matt Raible")
     }
     def destroy = {
     }
